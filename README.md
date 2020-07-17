c6f72797a1a9dc7514a27619c3f66d03eac4bf1c
chany-ahn <chany.kima@gmail.com>
2020-07-17 19:55:39 -0400

    Fixed up code and added some print statements

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # PokemonTypeScraper
 
-This is a basic Web Scraper which asks for the Pokemon type you want to see as input (e.g. fire) and displays all the Pokemon that have that type.
+This is a basic Web Scraper which asks for the Pokemon type you want to see as input (e.g. fire) and displays all the Pokemon that have that type. This script utlizes the library BeautifulSoup4 to parse the data easily.
 
 The website I am scraping off of is: https://bulbapedia.bulbagarden.net/wiki/List_of_Pok%C3%A9mon_by_National_Pok%C3%A9dex_number
--- TypeScraper.py
+++ TypeScraper.py
@@ -37,10 +37,6 @@ types = ["bug",
 "water"]
 
 pokemonList = soup.find_all('tr', style="background:#FFF")
-# pokemon = pokemonList[0].contents
-# pokemon1 = pokemonList[3].contents
-# print(pokemon)
-# print(pokemon1)
 # where index 7 holds the name 9 holds the type
 monoType = []
 # where index 9 and 11 hold the types
@@ -61,7 +57,7 @@ for i in range(len(pokemonList)):
 while True:
     # Take user input to see what type they want
     findType = input("What Pokemon type are you looking for? ")
-    findType.lower()
+    findType = findType.lower()
     if findType in types:
         break
     print("Invalid, please enter another type")
@@ -70,24 +66,28 @@ findPokemonMono = []
 findPokemonDual = []
 
 for i in range(len(monoType)):
+    # find the mono-typed pokemon with desired type
     pokemonType = monoType[i][9].a.span.text
     if pokemonType.lower() == findType:
         # append the contents (for now)
         findPokemonMono.append(monoType[i])
 
 for i in range(len(dualType)):
+    # find dual-type pokemon with desired type
     pokemonType1 = dualType[i][9].a.span.text
     pokemonType2 = dualType[i][11].a.span.text
-
     if pokemonType1.lower() == findType or pokemonType2.lower() == findType:
         findPokemonDual.append(dualType[i])
 
+# print out the pokemon
 print("Mono-Type Pokemon")
 for i in range(len(findPokemonMono)):
-    print(findPokemonMono[i][7].a.text)
+    print(findPokemonMono[i][7].a.text + " " + findPokemonMono[i][9].a.span.text)
 
 print()
 
 print("Dual-Type Pokemon")
 for i in range(len(findPokemonDual)):
-    print(findPokemonDual[i][7].a.text)
+    print(findPokemonDual[i][7].a.text + " " + findPokemonDual[i][9].a.span.text + "/" + findPokemonDual[i][11].a.span.text)
+
+print("Total number of " + findType + " type Pokemon is: " + str(len(findPokemonDual) + len(findPokemonMono)))
